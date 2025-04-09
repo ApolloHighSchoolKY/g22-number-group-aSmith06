8edb4e9f85015ff674ea979dd3e6b5f9460142a2
aSmith06 <andrew.smith2@stu.daviess.kyschools.us>
2025-04-09 13:29:02 +0000

    FRQ done

--- MultipleGroups.java
+++ MultipleGroups.java
@@ -1,1 +1,25 @@
+import java.util.List;
 
+public class MultipleGroups implements NumberGroup
+{
+
+    private List<NumberGroup> groupList;
+
+    public MultipleGroups(NumberGroup range1, NumberGroup range2, NumberGroup range3)
+    {
+        groupList = new List<NumberGroup>();
+
+        groupList.add(range1);
+        groupList.add(range2);
+        groupList.add(range3);
+    }
+
+    public boolean contains(int num)
+    {
+        for(NumberGroup range : groupList){
+            if(range.contains(num))
+                return true;
+        }
+        return false;
+    }
+}
--- Range.java
+++ Range.java
@@ -1,1 +1,24 @@
 
+public class Range implements NumberGroup
+{
+
+    private int[] betweens;
+
+    public Range (int lower, int upper)
+    {    
+        betweens = new int[upper - lower + 1]
+
+        for (int i = lower; i <= upper; i++){
+            betweens[i] = i;
+        }
+    }
+
+    public boolean contains(int num)
+    {
+        for(int i:betweens){
+            if(i==num || i = num % 10)
+                return true;
+        }
+        return false;
+    }
+}
